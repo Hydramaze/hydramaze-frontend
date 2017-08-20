0a9c6353d2984d8f6d791f0d4f315f6ecb05accb
msreis66 <msreis.66@gmail.com>
2017-08-20 18:45:54 -0300

    Printa algoritmos

--- src/app/components/algorithms-list/algorithms-list-controller.js
+++ src/app/components/algorithms-list/algorithms-list-controller.js
@@ -11,81 +11,27 @@ angular.module('hydramaze')
 
     console.log('Algorithms list has been loaded');
 
-    // $scope.fields = {
-    //     "Supervisionado": [
-    //       {
-    //         name: "Regressão",
-    //         algorithms: ["K-means", "SOM"]
-    //       },
-    //       {
-    //         name: "Classificação",
-    //         algorithms: ["K-means2", "SOM2"]
-    //       }
-    //     ],
-    //     "Não Supervisionado": [
-    //       {
-    //         name: "Associação",
-    //         algorithms: ["K-means3", "SOM3"]
-    //       },
-    //       {
-    //         name: "Agrupamento",
-    //         algorithms: ["K-means4", "SOM4"]
-    //       }
-    //     ]
-    //   };
     $http.get('http://localhost:8080/api/algorithm')
       .then(function(response) {
-        $scope.algorithms = response.data;
-        $scope.learningTypes = categories($scope.algorithms);
-        $scope.types = algorithmTypes($scope.algorithms);
-        // algorithmsStructure($scope.algorithms);
-        console.log(response.data);
-        // formatData(response.data);
+        $scope.algorithms = formatterData(response.data);
+        console.log("churros: ", $scope.algorithms);
       });
 
-    function categories(algorithms) {
-      var learningTypes = {};
-
-      // var dict = {}; dict.key1 = "value1"; dict.key2 = "value2";
-      angular.forEach(algorithms, function(value, key) {
-        if (!learningTypes.hasOwnProperty(value.learningType)) {
-          // learningTypes.push(value);
-          // learningTypes.push({ key: value.learningType, value: ["b"] });
-          learningTypes.value.learningType = value;
-        }
-      });
-      console.log("learningTypes: ",learningTypes);
-      return learningTypes;
-    };
-
-    function algorithmTypes(algorithms) {
-      var types = [];
-      angular.forEach(algorithms, function(value, key) {
-        if ($.inArray(value.type, types) == -1) {
-          types.push(value.type);
-        }
-      });
-      console.log("types: ",types);
-      return types;
-    };
-
-    // function algorithmsStructure(algorithms) {
-    //   var algorithm = [];
-    //   var json = [];
-    //   // angular.forEach($scope.types, function(typeName, typeKey) {
-    //   //   algorithm.push(typeName);
-    //   // });
-    //   angular.forEach(algorithms, function(value, key) {
-    //     algorithm[value.type] = value;
-    //   });
-    //   console.log("Churros: ", algorithm);
-    // }
-    //
-    // function formatData(algorithms) {
-    //   angular.forEach(algorithms, function(value, key) {
-    //
-    //   });
-    // }
-    //
-    // function
-  });
+    function formatterData(algorithms) {
+     var algorithmsObj = {};
+     angular.forEach(algorithms, function(value, key) {
+       if (!algorithmsObj[value.learningType]) {
+         algorithmsObj[value.learningType] = {};
+       }
+       if (!algorithmsObj[value.learningType][value.type]) {
+         algorithmsObj[value.learningType][value.type] = [];
+       }
+       algorithmsObj[value.learningType][value.type].push({
+         id: value.id, name: value.name,
+         completeDescription: value.completeDescription,
+         simpleDescription: value.simpleDescription
+       });
+     });
+     return algorithmsObj;
+   };
+});
