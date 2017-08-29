17c8949beb6cbc4b65b98a7e38515e5ae1712ea6
msreis66 <msreis.66@gmail.com>
2017-08-29 20:30:06 -0300

    Integração api de parametros

--- src/app/views/laboratory-content/tutorial/step-two/step-two-controller.js
+++ src/app/views/laboratory-content/tutorial/step-two/step-two-controller.js
@@ -7,9 +7,8 @@
  */
 
 angular.module('hydramaze')
-  .controller('StepTwoCtrl', function($scope, $http, $timeout) {
+  .controller('StepTwoCtrl', function($scope, $http, $timeout, $compile) {
 
-    console.log("StepTwoCtrl Controller as been loaded!");
 
     $scope.init = function() {
       $scope.algorithmId = localStorage.getItem("value");
@@ -18,6 +17,41 @@ angular.module('hydramaze')
       $http.get("http://localhost:8080/api/parameter/getByAlgorithmId?id=" + $scope.algorithmId).
       then(function(response) {
         $scope.algorithm = response.data;
+        $scope.tutorial.StepTwoCtrl = defineParameters($scope.algorithm);
+
+        angular.forEach($scope.tutorial.StepTwoCtrl, function (val, key) {
+
+          var newBlock = document.createElement("div");
+          newBlock.setAttribute("id", "component-" + key);
+          newBlock.setAttribute("class", "block container-fluid");
+
+          var content = document.createElement(val.component);
+          content.setAttribute("data", val.data);
+          newBlock.append(content);
+
+          $('#step-two-content').append(newBlock);
+
+        });
+
+        $compile($('#step-two-content').contents())($scope);
       });
     });
+    console.log("StepTwoCtrl Controller as been loaded!");
+
   });
+
+function defineParameters(algorithm) {
+  var parameters = [];
+  $.each(algorithm, function(key, value) {
+    parameters.push(value.component);
+  });
+  return getStepTwoConfiguration(parameters);
+}
+
+function getStepTwoConfiguration(parameters) {
+  var screenConfiguration = [];
+  $.each(parameters, function(key, value) {
+    screenConfiguration.push(value + "-directive");
+  });
+  return screenConfiguration;
+}
