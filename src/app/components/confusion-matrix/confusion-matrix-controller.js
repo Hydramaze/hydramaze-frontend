5fb810669435378c8af505dc514c13dc58308ec2
msreis66 <msreis.66@gmail.com>
2017-10-20 22:44:25 -0200

    Componente da matriz de confusão

---
+++ src/app/components/confusion-matrix/confusion-matrix-controller.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name hydramaze.controller:confusionMatrixCtrl
+ * @description Confusion Matrix Controller.
+ */
+
+angular.module('hydramaze')
+  .controller('confusionMatrixCtrl', function($scope) {
+  });
